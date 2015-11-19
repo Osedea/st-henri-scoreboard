1edd09fbb012810c851a5a14801503305e8fabb4
Silverak925 <silverak925@gmail.com>
2015-11-19 17:55:11 -0500

    Add advice for next time

--- timer.js
+++ timer.js
@@ -1,9 +1,14 @@
 
+
 var time = function() {
 
 
+	for(var sec=60; sec < -1; sec--);
+	for(var min=60; min < -1; min--);
 
+	//Prochaine fois, remplacer focntion for par setInterval (lien:http://www.xul.fr/ecmascript/settimeout.php/)//
 
-}
+};
 
-console.log("salut!");
+console.log(sec, min);
+ 
