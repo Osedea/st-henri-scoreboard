1ede05795b4092fbb578c66a0f5a276fa1b9e3ea
Silverak925 <silverak925@gmail.com>
2015-11-24 16:04:36 -0500

    try something smart

--- timer.js
+++ timer.js
@@ -3,12 +3,12 @@
 var time = function() {
 
 var sec = 0;
-var sec = setInterval("afficherSec",1000);
+var sec = setInterval(afficherSec(console.log(sec, min),1000);
 
 	//Prochaine fois, remplacer focntion for par setInterval (lien:http://www.xul.fr/ecmascript/settimeout.php/)//
 clearInterval(sec)
 
 };
 
-console.log(sec, min);
+
  
