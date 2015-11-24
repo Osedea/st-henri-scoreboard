4bfb73126f9ee0cfd2f17da7ceb77d862202edd2
Silverak925 <silverak925@gmail.com>
2015-11-24 15:51:19 -0500

    add value sec

--- timer.js
+++ timer.js
@@ -2,6 +2,7 @@
 
 var time = function() {
 
+var sec = 0;
 var sec = setInterval("afficherSec",1000);
 
 	//Prochaine fois, remplacer focntion for par setInterval (lien:http://www.xul.fr/ecmascript/settimeout.php/)//
