ed92a920a4b45ed062791952ff200d52725d7791
Silverak925 <silverak925@gmail.com>
2015-11-24 15:43:01 -0500

    remove old function and add Real function

--- timer.js
+++ timer.js
@@ -2,11 +2,10 @@
 
 var time = function() {
 
-
-	for(var sec=60; sec < -1; sec--);
-	for(var min=60; min < -1; min--);
+var sec = setInterval("afficherSec",1000);
 
 	//Prochaine fois, remplacer focntion for par setInterval (lien:http://www.xul.fr/ecmascript/settimeout.php/)//
+clearInterval(sec)
 
 };
 
